d05e173d82357bd3c2c0026239452e382c9b33c7
Md_Shafiul_Alam <shafiul.alam@bjitgroup.com>
2023-03-30 11:30:33 +0600

    Initial Commit

---
+++ Study-CoreML-SwiftUI/Study_CoreML_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_CoreML_SwiftUIApp.swift
+//  Study-CoreML-SwiftUI
+//
+//  Created by ShafiulAlam-00058 on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Study_CoreML_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
